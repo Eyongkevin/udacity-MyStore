a6ec297f555de215429e88da5d452c29f5e62957
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2021-02-28 10:48:46 +0100

    feats: Link product list and item details with router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductListComponent},
+  {path: 'item-detail/:id', component: ProductItemDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
